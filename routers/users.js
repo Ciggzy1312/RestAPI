fec74ea14844208481a3a6681005eca45b9743bd
Deepayan <deepayanmukherjee1312@gmail.com>
2021-07-22 12:16:20 +0530

    added patch method to update data

--- routers/users.js
+++ routers/users.js
@@ -27,6 +27,21 @@ router.delete('/:id', (req,res)=>{
     const {id} = req.params;
 
     users = users.filter((user)=> user.id!==id)
+    console.log('Data deleted')
+})
+
+router.patch('/:id', (req,res)=>{
+    const user = users.find((user)=> user.id===req.params.id)
+    const {name,age} = req.body;
+
+    if(name){
+        user.name = name;
+    }
+    if(age){
+        user.age = age;
+    }
+
+    console.log('Data updated')
 })
 
 module.exports = router;
